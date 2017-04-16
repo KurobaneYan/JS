e4fa6a9c2c224b22043e286763d43ed0a9f1f285
KurobaneYan <yan.kurobane@gmail.com>
2017-04-16 20:21:33 +0300

    add version with button which replaces itself with form

--- 5/js/script2.js
+++ 5/js/script2.js
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Task 5</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <form class="form-horizontal">
+$(document).ready(function() {
+    $('.bsuFaculties').hide();
+    $('#university').change(function() {
+        switch($(this).val()) {
+            case 'BSU':
+                $('.bsuFaculties').show();
+                $('.bsuirFaculties').hide();
+                $('#faculty').val('Faculty of Law');
+                break;
+            case 'BSUIR':
+                $('.bsuFaculties').hide();
+                $('.bsuirFaculties').show();
+                $('#faculty').val('Faculty of Computer Systems and Networks');
+                break;
+        }
+    });
+});
+
+
+getById('showForm').onclick = () => {
+    getById('form').innerHTML = `<form class="form-horizontal">
             <div class="form-group">
                 <label for="firstName" class="col-sm-2 control-label">First name</label>
                 <div class="col-sm-10">
@@ -38,6 +50,9 @@
                     </label>
                 </div>
             </div>
+
+
+
             <div class="form-group">
                 <label for="university" class="col-sm-2 control-label">University</label>
                 <div class="col-sm-10">
@@ -92,11 +107,68 @@
                     <button id="submit" type="submit" class="btn btn-default">Submit</button>
                 </div>
             </div>
-        </form>
-    </div>
+        </form>`;
+    getById('submit').onclick = () => {alert(createMessage())};
+};
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-<script src="js/script.js"></script>
-</body>
-</html>
+function getById(id) {
+    return document.getElementById(id);
+}
+
+function getByName(name) {
+    return document.getElementsByName(name);
+}
+
+function getByClass(name) {
+    return document.getElementsByClassName(name);
+}
+
+function getCheckedItem(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].checked) {
+            return array[i].value;
+        }
+    }
+}
+
+function getSex() {
+    return getCheckedItem(getByName('sex'));
+}
+
+function getFullName() {
+    let firstName = getById('firstName').value;
+    let patronymic = getById('patronymic').value;
+    let lastName = getById('lastName').value;
+    return firstName + ' ' + patronymic + ' ' +
+        lastName;
+}
+
+function getUniversityInfo() {
+    let university = getById('university').value;
+    let faculty = getById('faculty').value;
+    let course = getById('course').value;
+    let group = getById('group').value;
+    let distanceEducation = getById('distanceEducation').checked;
+    let universityInfo = university + ', ' + faculty + ', ' + 
+        course + ' course, group: ' + group;
+    if (distanceEducation) {
+        universityInfo += ' on a distance education.';
+    } else {
+        universityInfo += ' regulary attend university.';
+    }
+    return universityInfo;
+}
+
+function createMessage() {
+    let about = getById('about').value;
+    let sex = getSex();
+    let message = '';
+    if (sex == 'male') {
+        message += 'His name is ' + getFullName() + '.\nHe';
+    } else {
+        message += 'Her name is ' + getFullName() + '.\nShe';
+    }
+    message += ' study at ' + getUniversityInfo() + 
+        '\nSome information about that person: ' + about; 
+    return message;
+}
